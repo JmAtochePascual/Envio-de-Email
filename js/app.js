b7f185eaf6e35c2c80ab80fb1e6957b87de9e963
JeanCarlos <jmatochepascual@gmail.com>
2024-04-27 16:55:05 -0500

    [FEATURE] eliminar alerta

--- js/app.js
+++ js/app.js
@@ -32,6 +32,8 @@ const validarCampo = (event) => {
     mostrarAlerta(elementoHTML, `Este campo ${event.target.id} es obligatorio`);
     return;
   };
+
+  elimiinarAlerta(elementoHTML);
 };
 
 
@@ -39,11 +41,26 @@ const validarCampo = (event) => {
 
 
 const mostrarAlerta = (elementoHTML, mensaje, tipo) => {
+
+  elimiinarAlerta(elementoHTML);
+
   const alerta = document.createElement('p');
   alerta.textContent = mensaje;
-  alerta.classList.add('text-white', 'p-2', 'mt-5', 'text-center', 'error');
+  alerta.classList.add('text-white', 'p-2', 'mt-5', 'text-center');
 
-  tipo ? alerta.classList.add('bg-green-500') : alerta.classList.add('bg-red-600');
+  tipo ? alerta.classList.add('bg-green-500', 'error') : alerta.classList.add('bg-red-600', 'succes');
 
   elementoHTML.appendChild(alerta);
+};
+
+
+
+
+
+const elimiinarAlerta = (elementoHTML) => {
+
+  const alertaElement = elementoHTML.querySelector('.error') || elementoHTML.querySelector('.succes');
+
+  if (alertaElement) alertaElement.remove();
+
 };
