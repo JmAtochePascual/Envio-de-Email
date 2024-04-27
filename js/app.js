f58137c5d3a992e80ce17564fdb7c378ccf77323
JeanCarlos <jmatochepascual@gmail.com>
2024-04-27 16:38:33 -0500

    [FEATURE] mostrar alerta

--- js/app.js
+++ js/app.js
@@ -26,9 +26,24 @@ document.addEventListener('DOMContentLoaded', () => {
 const validarCampo = (event) => {
 
   const valorInput = event.target.value.trim();
+  const elementoHTML = event.target.parentElement;
 
   if (valorInput === "") {
-    console.log('Campo vacio');
+    mostrarAlerta(elementoHTML, `Este campo ${event.target.id} es obligatorio`);
     return;
   };
+};
+
+
+
+
+
+const mostrarAlerta = (elementoHTML, mensaje, tipo) => {
+  const alerta = document.createElement('p');
+  alerta.textContent = mensaje;
+  alerta.classList.add('text-white', 'p-2', 'mt-5', 'text-center', 'error');
+
+  tipo ? alerta.classList.add('bg-green-500') : alerta.classList.add('bg-red-600');
+
+  elementoHTML.appendChild(alerta);
 };
