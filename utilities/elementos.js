55ce47498fbf78df807c99e2c8c4e0b8c03478da
JeanCarlos <jmatochepascual@gmail.com>
2024-07-23 18:41:44 -0500

    [ADD] registrar elementos

---
+++ utilities/elementos.js
@@ -0,0 +1,16 @@
+const inputEmailElement = document.querySelector('#email');
+const inputAsuntoElement = document.querySelector('#asunto');
+const inputMensajeElement = document.querySelector('#mensaje');
+const formularioElement = document.querySelector('#formulario');
+const botonEnviarElement = document.querySelector('#enviar');
+const botonResetElement = document.querySelector('#resetear');
+
+
+export {
+  inputEmailElement,
+  inputAsuntoElement,
+  inputMensajeElement,
+  formularioElement,
+  botonEnviarElement,
+  botonResetElement
+}
