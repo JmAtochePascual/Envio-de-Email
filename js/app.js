d1f930915f7b9d3e97551c4f2666b804ae3b266b
JeanCarlos <jmatochepascual@gmail.com>
2024-07-23 18:59:28 -0500

    [FEATURE] Validar si todos los campos del formulario estan llenos

--- js/app.js
+++ js/app.js
@@ -1,8 +1,14 @@
-import { habilitarBotonEnviar } from "../utilities/funciones.js";
+import { inputAsuntoElement, inputEmailElement, inputMensajeElement } from "../utilities/elementos.js";
+import { habilitarBotonEnviar, validarCampos } from "../utilities/funciones.js";
 
 
 
 // Eventos
 document.addEventListener('DOMContentLoaded', () => {
   habilitarBotonEnviar(false);
+
+  inputEmailElement.addEventListener('input', validarCampos);
+  inputAsuntoElement.addEventListener('input', validarCampos);
+  inputMensajeElement.addEventListener('input', validarCampos);
+
 });
--- utilities/funciones.js
+++ utilities/funciones.js
@@ -1,14 +1,23 @@
-import { botonEnviarElement } from './elementos.js';
+import { botonEnviarElement, inputAsuntoElement, inputEmailElement, inputMensajeElement } from './elementos.js';
 
 
-// Funcion para habilitar o deshabilitar el boton de enviar
+// Validar si todos los campos del formulario estan llenos
+const validarCampos = () => {
+  const emialValor = inputEmailElement.value.trim();
+  const asuntoValor = inputAsuntoElement.value.trim();
+  const mensajeValor = inputMensajeElement.value.trim();
+
+  [emialValor, asuntoValor, mensajeValor].every(valor => valor !== '') ? habilitarBotonEnviar(true) : habilitarBotonEnviar(false);
+}
+
+
+// Habilitar o deshabilitar el boton de enviar
 const habilitarBotonEnviar = (estado) => {
   botonEnviarElement.disabled = estado ? false : true;
-  estado
-    ? botonEnviarElement.classList.remove('opacity-50')
-    : botonEnviarElement.classList.add('opacity-50');
+  estado ? botonEnviarElement.classList.remove('opacity-50') : botonEnviarElement.classList.add('opacity-50');
 }
 
 export {
-  habilitarBotonEnviar
+  habilitarBotonEnviar,
+  validarCampos
 }
