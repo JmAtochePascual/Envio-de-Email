60b9e81c052e70dac6c33d2654c53503a49e7064
JeanCarlos <jmatochepascual@gmail.com>
2024-07-23 19:02:42 -0500

    [UPDATE] Crear un nuevo objeto Email

--- js/app.js
+++ js/app.js
@@ -1,6 +1,16 @@
-import { inputAsuntoElement, inputEmailElement, inputMensajeElement } from "../utilities/elementos.js";
+import Email from "../utilities/Email.js";
+import { formularioElement, inputAsuntoElement, inputEmailElement, inputMensajeElement } from "../utilities/elementos.js";
 import { habilitarBotonEnviar, validarCampos } from "../utilities/funciones.js";
 
+// Enviar mensaje
+const enviarMensaje = (vent) => {
+  vent.preventDefault();
+
+  // Crear un nuevo objeto Email
+  const email = new Email(inputEmailElement.value.trim(), inputAsuntoElement.value.trim(), inputMensajeElement.value.trim());
+
+
+}
 
 
 // Eventos
@@ -10,5 +20,7 @@ document.addEventListener('DOMContentLoaded', () => {
   inputEmailElement.addEventListener('input', validarCampos);
   inputAsuntoElement.addEventListener('input', validarCampos);
   inputMensajeElement.addEventListener('input', validarCampos);
+
+  formularioElement.addEventListener('submit', enviarMensaje);
 
 });
